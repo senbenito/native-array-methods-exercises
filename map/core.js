d2bac98c764baba877aac10546a08820914e62fd
Shannon Rivers <shannon.rivers@gmail.com>
2017-04-25 10:23:05 -0600

    trying vowels

--- map/core.js
+++ map/core.js
@@ -1,14 +1,25 @@
 function multiplyBy10 (array) {
- // your code here
-};
+  var result = array.map(function (element){
+    return element*10;
+  });
+  return result;
+}
 
 function shiftRight (array) {
- // your code here
-};
+  return array.map(function (element, i, arr){
+    if (i === 0){
+      return arr[arr.length-1];
+    } return arr[i-1];
+  });
+}
 
 function onlyVowels (array) {
- // your code here
-};
+  var vowels = array.map(function(elem, index) {
+    if (elem === 'a' || elem === 'e' || elem === 'i' || elem === 'o' || elem === 'u'){
+      return elem;
+    } return 'not a vowel'
+  }); return vowels;
+}
 
 function doubleMatrix (array) {
  // your code here

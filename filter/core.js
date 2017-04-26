160bcc0252c254814630869e14762177790d9e35
Shannon Rivers <shannon.rivers@gmail.com>
2017-04-26 09:54:39 -0600

    all pass

--- filter/core.js
+++ filter/core.js
@@ -1,18 +1,42 @@
 function onlyEven (array) {
-  // your code here
-};
+  let results = array.filter(function (element){
+    return element%2 ===0;
+  });
+  return results;
+}
 
 function onlyOneWord (array) {
-  // your code here
-};
+  let results = array.filter(function(element){
+    return !element.includes(" ");
+  });
+  return results;
+}
 
 function positiveRowsOnly (array) {
-  // your code here
-};
+  let results = array.filter(function (element){
+    if (element.sort()[0]>0){
+      return element;
+    }
+  });
+  return results;
+}
 
 function allSameVowels (array) {
-  // your code here
-};
+  let results = array.filter(function (element){
+    var vowelArray = [];
+    for (let i=0; i<element.length; i++){
+      if (element[i] === 'a' || element[i] === 'e' || element[i] === 'i' || element[i] === 'o' || element[i] === 'u'){
+        vowelArray.push(element[i]);
+      }
+    }
+    for (let j=0; j<vowelArray.length; j++){
+      if (vowelArray[j] !== vowelArray[j+1]){
+        return;
+      } return element;
+    }
+  });
+  return results;
+}
 
 module.exports = {
   onlyEven: onlyEven,
--- map/core.js
+++ map/core.js
@@ -6,19 +6,23 @@ function multiplyBy10 (array) {
 }
 
 function shiftRight (array) {
-  return array.map(function (element, i, arr){
-    if (i === 0){
+  return array.map(function (element, index, arr){
+    if (index === 0){
       return arr[arr.length-1];
-    } return arr[i-1];
+    } return arr[index-1];
   });
 }
 
 function onlyVowels (array) {
   var vowels = array.map(function(elem, index) {
-    if (elem === 'a' || elem === 'e' || elem === 'i' || elem === 'o' || elem === 'u'){
-      return elem;
-    } return 'not a vowel'
-  }); return vowels;
+    for (let i=0; i<elem.length; i++){
+      var elemVowels = '';
+    if (elem[i] === 'a' || elem[i] === 'e' || elem[i] === 'i' || elem[i] === 'o' || elem[i] === 'u') {
+      elemVowels += elem[i];
+    }
+
+  }
+}); return vowels;
 }
 
 function doubleMatrix (array) {

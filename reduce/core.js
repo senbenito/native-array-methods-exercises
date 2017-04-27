993a79ff198c7bddcac296e8fa680637feff5523
Shannon Rivers <shannon.rivers@gmail.com>
2017-04-27 15:30:52 -0600

    cleared reduce

--- reduce/core.js
+++ reduce/core.js
@@ -1,18 +1,58 @@
 function sum (array) {
-  // your code here
-};
+  return array.reduce(function(acc, val) {
+  return acc + val;
+}, 0);
+}
 
-function productAll (array) {
-  // your code here
-};
 
-function objectify (array) {
-  // your code here
-};
+function productAll (array){
+  var subResult = array.reduce(function(acc, curr) {
+  return acc.concat(curr);
+  });
+  for (let j=0; j<subResult.length; j++){
+    if (j===0){
+      results = subResult[j];
+    } else {
+      results *= subResult[j];
+    }
+  }
+return results;
+}
+
+// function objectify (array) {
+//   console.log(array);
+//   results = {};
+//   for (let i=0; i<array.length; i++){
+//     var innerArray = array[i];
+//     for (let j=0; j<innerArray.length-1; j++){
+//       results[innerArray[j]] = innerArray[j+1];
+//     }
+//   }
+//   return results;
+// }
+
+function objectify(array){
+  var results = array.reduce(function (acc, curr){
+    acc[curr[0]]=curr[1];
+    return acc;
+  }, {});
+  return results;
+}
 
 function luckyNumbers (array) {
-  // your code here
-};
+// console.log(array);
+return array.reduce(function (acc, curr, index){
+  console.log(index);
+  console.log(acc);
+    if (index === 0){
+      return acc + curr;
+    } else if (index !== array.length-1){
+      return acc + ', ' + curr;
+    } else {
+      return acc + ', and ' + curr;
+    }
+  }, "Your lucky numbers are: ");
+}
 
 module.exports = {
   sum: sum,
